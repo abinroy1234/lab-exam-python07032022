922fdfd872c7fdf0817110a8f77f840fa72e4d9e
abinroy1234 <94526037+abinroy1234@users.noreply.github.com>
2022-03-07 12:58:30 +0530

    Add files via upload

---
+++ lab abin a2.py
@@ -0,0 +1,19 @@
+string=input("enter a string")
+l1=[]
+l2=[]
+letter=[]
+size=int(input("enter the size of the string"))
+uppercase={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
+lowercase={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+
+letter=string.split
+for i in range(0,size):
+    letter.append(size)
+for letter in string:
+    if uppercase in string:
+        l1.append(letter)
+        print("upper case in string are",l1)
+for letter in string:
+    if lowercase in string:
+        l2.append(letter)
+    print("lowercase in string are",l2)
